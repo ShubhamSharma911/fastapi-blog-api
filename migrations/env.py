feefcb20c46070d0d85526d073a874011708551a
shubham-sharma <shubham-sharma@sadmins-MacBook-Air.local>
2025-05-20 15:56:53 +0530

    docker-compose.yml && bind-Mount

--- migrations/env.py
+++ migrations/env.py
@@ -1,14 +1,14 @@
 from logging.config import fileConfig
-from app.models import User, Post, Vote
+
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+
 from alembic import context
-from app.models import Base
-from app.config import settings
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}')
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -18,8 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
-print("Tables Alembic sees:", target_metadata.tables.keys())
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
